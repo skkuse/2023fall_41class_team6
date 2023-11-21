fcea72dd42fa73eb599e301ea06a245d34a0eff9
Hyeonminlee73 <133738567+Hyeonminlee73@users.noreply.github.com>
2023-11-22 08:13:45 +0900

    Create Before.java

---
+++ green_pattern/Row-major_than_Column-major/Before.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+public class Before {
+    public static void main(String[] args) {
+    long totalSum = 0;
+    for (int p = 0 ; p < 1000 ; p++){
+        int rows = 1000;
+        int cols = 1000;
+        int[][] array = new int[rows][cols];
+        Random random = new Random();
+
+        // 배열에 값 할당
+        for (int k = 0; k < cols; k++) {
+            for (int i = 0; i < rows; i++) {
+                array[i][k] = random.nextInt(100);
+                }
+            }
+            
+            for (int k = 0; k < cols; k++) {
+                for (int i = 0 ; i < rows ; i++){
+                    totalSum += array[i][k];
+                }
+
+            }
+        }
+        System.out.println(totalSum);
+    }
+}
