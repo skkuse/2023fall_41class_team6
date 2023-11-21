f1d760eb40543f799c42b0d63d9847b23bb5a561
Hyeonminlee73 <133738567+Hyeonminlee73@users.noreply.github.com>
2023-11-22 06:13:26 +0900

    Create Before.java

---
+++ green_pattern/Arrays_parallelsort/Before.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Before {
+    
+    public static void main(String[] args) {
+        for (int k = 0 ; k < 1000 ; k++){
+	        int[] data = new int[1000000];
+	        Random random = new Random();
+
+        for (int i = 0; i < data.length; i++) {
+            data[i] = random.nextInt(1000000);
+        }
+        Arrays.sort(data);
+        }
+    }
+}
