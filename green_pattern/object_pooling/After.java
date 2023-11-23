94269d65640a2b8e07d64f10adb8fc3cfe5779f4
Changooo <cgl1234321@gmail.com>
2023-11-24 00:55:28 +0900

    [edit] rollback object_pooling (mistakenly overwritten)

--- green_pattern/object_pooling/After.java
+++ green_pattern/object_pooling/After.java
@@ -1,44 +1,30 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 
-class MyObject {
-    private int value;
-    public MyObject(int value) {
-        System.out.println("This Takes so long...");
-        System.out.println("This Takes so long...");
-        System.out.println("This Takes so long...");
-        this.value = value;
-    }
-}
+public class After {
+    public static class ObjectPool {
+        private static final int MAX_POOL_SIZE = 3;
+        private static Queue<ArrayList<Object>> pool = new LinkedList<>();
 
-class ObjectPoolExample {
-    private static final int POOL_SIZE = 1;
-    private static List<MyObject> objectPool = new ArrayList<>(POOL_SIZE);
+        public static Object getObject() {
+            if(pool.isEmpty()) {
+                return new Object();
+            } else {
+                return pool.poll();
+            }
+        }
 
-    static {
-        initializeObjectPool();
-    }
-
-    private static void initializeObjectPool() {
-        for (int i = 0; i < POOL_SIZE; i++) {
-            objectPool.add(new MyObject(0));
+        public static void returnObject(ArrayList<Object> object) {
+            if(pool.size() < MAX_POOL_SIZE) {
+                pool.add(object);
+            }
         }
     }
 
-    public static MyObject getObjectFromPool() {
-        return objectPool.remove(0);
-    }
-
-    public static void returnObjectFromPool(MyObject obj) {
-        objectPool.add(obj);
-    }
-}
-public class After {
-    public static void main(String[] args) {
-        int iterations = 100000;
-        for (int i = 0; i < iterations; i++) {
-            MyObject obj = ObjectPoolExample.getObjectFromPool();
-            ObjectPoolExample.returnObjectFromPool(obj);
+    public static void main(String[] args){
+        for(int i=0; i<100000; i++) {
+            ObjectPool.getObject();
         }
     }
 }
--- green_pattern/object_pooling/Before.java
+++ green_pattern/object_pooling/Before.java
@@ -1,17 +1,9 @@
-class MyObject {
-    private int value;
-    public MyObject(int value) {
-        System.out.println("This Takes so long...");
-        System.out.println("This Takes so long...");
-        System.out.println("This Takes so long...");
-        this.value = value;
-    }
-}
+import java.util.ArrayList;
+
 public class Before {
-    public static void main(String[] args) {
-        int iterations = 100000;
-        for (int i = 0; i < iterations; i++) {
-            MyObject obj = new MyObject(0);
+    public static void main(String[] args){
+        for(int i=0; i<100000; i++) {
+            new ArrayList<>(i);
         }
     }
 }
